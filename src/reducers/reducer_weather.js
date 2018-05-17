570aff7e7148325c7e4d125320ba540648f53a57
Chris Casey <cdcasey@gmail.com>
2018-05-17 16:41:47 -0500

    Fixed dumb type and added some error handling

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -4,12 +4,12 @@ export default function (state = [], action) {
 
     switch (action.type) {
         case FETCH_WEATHER:
-            console.log("ACTION:", action.payload.data);
-
-            // return state.concat([action.paylod.data]);
-            return [action.paylod.data, ...state];
-        // default:
-        //     break;
+            console.log("ACTION:", action);
+            if (action.error) {
+                break;
+            } else {
+                return [action.payload.data, ...state];
+            }
     }
     return state;
 }
